8c80c89043cf21be6d03924511d1b0c1c1cf0e57
blemu <birukmit@gmail.com>
2017-01-12 13:04:39 -0800

    service 3

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,7 +1,7 @@
 (function() {
-     function SongPlayer() {
+     function SongPlayer(Fixtures) {
         var SongPlayer = {};
-        var currentSong = null;
+       var currentAlbum = Fixtures.getAlbum();
          /**
  * @desc Buzz object audio file
  * @type {Object}
@@ -12,11 +12,11 @@
  * @desc Stops currently playing song and loads new audio file as currentBuzzObject
  * @param {Object} song
  */
-         
+       SongPlayer.currentSong = null;  
         var setSong = function(song) {
             if (currentBuzzObject) {
                 currentBuzzObject.stop();
-                currentSong.playing = null;
+                SongPlayer.currentSong.playing = null;
             }
 
             currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -24,8 +24,16 @@
                 preload: true
             });
 
-            currentSong = song;
+            SongPlayer.currentSong = song;
         };
+         
+          /**
+        * @desc Active song object from list of songs
+        * @type {Object}
+        */
+        SongPlayer.currentSong = null;  
+        
+         
         /**
         * @function playSong
         * @desc plays currently stoped song
@@ -35,15 +43,25 @@
              currentBuzzObject.play(); 
                  song.playing = true;
             }
-
+         
+         var getSongIndex = function(song) {
+                return currentAlbum.songs.indexOf(song);
+            };
+         
+                 /**
+        * @desc Active song object from list of songs
+        * @type {Object}
+        */
+         
          
          SongPlayer.play = function(song) {
-             if (currentSong !== song) {
+             song = song || SongPlayer.currentSong;
+             if (SongPlayer.currentSong !== song) {
                 setSong(song);
                 playSong(song);
                  
                
-             } else if (currentSong === song) {
+             } else if (SongPlayer.currentSong === song) {
                     if (currentBuzzObject.isPaused()) {
                         currentBuzzObject.play();
                     }
@@ -53,9 +71,24 @@
 
      
         SongPlayer.pause = function(song) {
+            song = song || SongPlayer.currentSong;
          currentBuzzObject.pause();
          song.playing = false;
         };
+         
+         SongPlayer.previous = function() {
+             var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+             currentSongIndex--;
+             
+             if (currentSongIndex < 0) {
+                 currentBuzzObject.stop();
+                 SongPlayer.currentSong.playing = null;
+                } else {
+                     var song = currentAlbum.songs[currentSongIndex];
+                     setSong(song);
+                     playSong(song);
+                    }
+            };
          
          return SongPlayer;
      }
